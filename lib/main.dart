b26eb5c03b282ba5ff421789c2c5fc8817be31a4
Ridhowalid01 <rialdifreedom01@gmail.com>
2025-05-24 12:08:04 +0700

    add location dark moon icon

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Sholat Reminder App',
+      debugShowCheckedModeBanner: false,
       home: HomeScreen()
     );
   }
--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -5,8 +5,42 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-
+    return Scaffold(
+      appBar: AppBar(
+        leadingWidth: 250, // atur lebar agar cukup luas
+        leading: Container(
+          color: Colors.red.withOpacity(0.3),
+          // warna transparan agar tetap bisa melihat isi
+          padding: const EdgeInsets.only(left: 16),
+          child: const Row(
+            children: [
+              Icon(
+                Icons.location_on,
+                color: Colors.blueAccent,
+                size: 24,
+              ),
+              SizedBox(width: 4),
+              Flexible(
+                child: Text(
+                  "Pamekasan, Jawa Timur",
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 16),
+            child: Icon(
+              Icons.dark_mode_outlined,
+              color: Colors.blueAccent,
+              size: 24,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
